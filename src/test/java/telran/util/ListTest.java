ade29364dfc4604833ba05b9eab71c19e36356a0
DaniilJustCode <gromdan90@gmail.com>
2024-08-20 18:39:49 +0300

    class work 20.08.24

---
+++ src/test/java/telran/util/ListTest.java
@@ -0,0 +1,14 @@
+package telran.util;
+
+abstract public class ListTest extends CollectionTest {
+    List<Integer> list;
+
+    @Override
+    void setUp() {
+        super.setUp();
+        list = (List<Integer>) collection;
+    }
+
+    //TODO's
+    //where list is the reference to collection being filled in the method setUp of CollectionTest class
+}
